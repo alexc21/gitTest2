46f09ed9c00ea285efe7dd47a68e541cd0757e0b
alexc21 <alexc210893@gmail.com>
2020-06-29 10:42:30 +0200

    C2.2 ajout prop formateur

--- src/metier/Formateur.java
+++ src/metier/Formateur.java
@@ -2,4 +2,25 @@ package metier;
 
 public class Formateur {
 
+	private String nom ;
+
+	public Formateur(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Formateur [nom=" + nom + "]";
+	}
+	
+	
 }
