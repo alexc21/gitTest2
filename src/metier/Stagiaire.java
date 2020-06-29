a995d054d677bc0f0fbce9296f4f5beefe296444
alexc21 <alexc210893@gmail.com>
2020-06-29 11:02:07 +0200

    C3.2 ajout prop stagiaire

--- src/metier/Stagiaire.java
+++ src/metier/Stagiaire.java
@@ -1,5 +1,25 @@
 package metier;
 
 public class Stagiaire {
+	private String nom;
 
+	public Stagiaire(String nom) {
+		super();
+		this.nom = nom;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Stagiaire [nom=" + nom + "]";
+	}
+	
+	
 }
